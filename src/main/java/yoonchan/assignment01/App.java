f26865b3b707401c95b7eaadd654a7e8ad4833b9
yoon <yoonchan.rhie@gmail.com>
2026-09-11 17:16:13 -0400

    Added enter functionality to key info label

--- src/main/java/yoonchan/assignment01/App.java
+++ src/main/java/yoonchan/assignment01/App.java
@@ -78,6 +78,9 @@ public class App extends Application {
                         button.setStyle(style);
                         keyInfoText.setText(key);
                         return;
+                    } else if (e.getCode() == KeyCode.ENTER) {
+                        keyInfoText.setText("");
+                        return;
                     }
 
                     keyInfoText.setStyle("-fx-text-fill: red");
