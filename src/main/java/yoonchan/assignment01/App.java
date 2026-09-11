6bc376a3dd811c783499408a088f3bc77da5a464
yoon <yoonchan.rhie@gmail.com>
2026-09-11 16:38:51 -0400

    Implemented event filters to update on-screen keyboard with pressed keys

--- src/main/java/yoonchan/assignment01/App.java
+++ src/main/java/yoonchan/assignment01/App.java
@@ -6,6 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -32,6 +34,65 @@ public class App extends Application {
         root.setTop(text);
 
         Scene scene = new Scene(root, 640, 480);
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            String style = KeyboardManager.getHeldKeyStyleString();
+
+            switch (e.getCode()) {
+                case SPACE:
+                    Button spaceButton = KeyboardManager.getButtonFromKey("SPACE");
+                    spaceButton.setStyle(style);
+
+                    e.consume(); // Prevents the space bar from triggering focused UI controls
+                    return;
+
+                case SHIFT:
+                    for (Button shift : KeyboardManager.getShiftKeys()) {
+                        shift.setStyle(style);
+                    }
+
+                    KeyboardManager.setPunctuationKeys(KeyboardManager.Case.UPPERCASE);
+                    return;
+
+                default:
+                    String key = e.getText().toUpperCase();
+                    if (KeyboardManager.hasKey(key)) {
+                        Button button = KeyboardManager.getButtonFromKey(key);
+                        button.setStyle(style);
+                    }
+                    break;
+            }
+        });
+
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, e -> {
+            String style = KeyboardManager.getReleasedKeyStyleString();
+
+            switch (e.getCode()) {
+                case SPACE:
+                    Button spaceButton = KeyboardManager.getButtonFromKey("SPACE");
+                    spaceButton.setStyle(style);
+
+                    e.consume(); // Prevents the space bar from triggering focused UI controls
+                    return;
+
+                case SHIFT:
+                    for (Button shift : KeyboardManager.getShiftKeys()) {
+                        shift.setStyle(style);
+                    }
+
+                    KeyboardManager.setPunctuationKeys(KeyboardManager.Case.LOWERCASE);
+                    return;
+
+                default:
+                    String key = e.getText().toUpperCase();
+                    if (KeyboardManager.hasKey(key)) {
+                        Button button = KeyboardManager.getButtonFromKey(key);
+                        button.setStyle(style);
+                    }
+                    break;
+            }
+        });
+
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
         stage.setScene(scene);
         stage.show();
